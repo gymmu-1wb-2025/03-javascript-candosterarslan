e436f34566d792dc0fe636d32c55f0ad79b7c0f7
Candost Erarslan <e228149@edu.sbl.ch>
2026-01-30 08:29:54 +0000

    .

--- src/exercises/ex-05.js
+++ src/exercises/ex-05.js
@@ -1,6 +1,6 @@
 const bodyHeight = Number(process.argv[2]); // cm
 
-// Formel (Maenner)
+// Formel (Männer)
 const talentScore = 243 - bodyHeight;
 
 if (talentScore < 50) {
@@ -9,7 +9,7 @@ if (talentScore < 50) {
 	console.log("Du musst dringend an deiner Sprungkraft arbeiten.");
 } else if (talentScore < 70) {
 	console.log(
-		"Wenn du schnell bist, kannst du dir die Libero-Position ueberlegen",
+		"Wenn du schnell bist, kannst du dir die Libero-Position überlegen",
 	);
 } else {
 	console.log("Du wirst im Volleyball nicht glücklich werden.");
